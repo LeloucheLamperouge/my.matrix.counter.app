bb63634d49d22bff6307906916b1e0e9ba47c549
LeloucheLamperouge <alexandrmujikov@gmail.com>
2017-01-09 15:00:46 +0200

    work

---
+++ sample/controllers/MatrixTableDataController.java
@@ -0,0 +1,16 @@
+package sample.controllers;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import sample.models.MatrixTableData;
+
+import javax.swing.text.TableView;
+
+/**
+ * Created by Lelouch on 07.12.2016.
+ */
+public class MatrixTableDataController {
+
+
+
+}
